73b135618d6c7dafa6d46ef6f9e6704421340c30
Sriraj-dev <Psriraj1902@gmail.com>
2021-11-02 09:32:21 +0530

    Initialised the Project

---
+++ lib/Screens/LoginPage.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'This is the LoginPage',
+          style: GoogleFonts.lato(
+            fontSize: 18,
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:delivery_app/Screens/LoginPage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: LoginPage(),
+  ));
+}
