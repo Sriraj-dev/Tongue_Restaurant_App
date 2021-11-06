441269e970e2cb11662de9c984094e9dcd6e691b
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2021-11-06 18:05:14 +0530

    added app bar

--- lib/Screens/homePage.dart
+++ lib/Screens/homePage.dart
@@ -1,19 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:flutter_svg/svg.dart';
 
-class LoginPage extends StatefulWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class homePage extends StatefulWidget {
+  const homePage({Key? key}) : super(key: key);
   @override
   _LoginPageState createState() => _LoginPageState();
 }
+
 //-------------------------------------------------jashwanth-------------------
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<homePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: SvgPicture.asset("assets/icons/menu.svg"),
+          onPressed: (){},
+        ),
+      ),
       body: Center(
         child: Text(
-          'crazy ',
+          'home ',
           style: GoogleFonts.lato(
             fontSize: 18,
           ),
