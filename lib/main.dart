21f60b4927d37eda15b73315320d971b3a6eea0f
Sriraj-dev <Psriraj1902@gmail.com>
2021-11-07 20:59:10 +0530

    Added the Api Services for authentication using mongodb

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:delivery_app/Screens/LoginPage.dart';
 import 'package:delivery_app/Screens/homePage.dart';
+import 'package:delivery_app/Services/storageServices.dart';
 import 'package:delivery_app/constants.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async{
+  bool isLogin = false;
+  // final details =await Storage().getData();
+  // if(details[0]!=null){
+  //   isLogin = true;
+  // }else{
+  //   isLogin = false;
+  // }
+  print('islogin is - $isLogin');
   runApp(
       MaterialApp(
         debugShowCheckedModeBanner: false,
@@ -15,6 +24,6 @@ void main() {
             bodyText2: TextStyle(color: ksecondaryColor ),
           ) ,
         ),
-        home: homePage(),
+        home: (isLogin)?homePage():LoginPage(),
   ));
 }
---
+++ lib/userModel.dart
@@ -0,0 +1,7 @@
+String token = '';
+
+class User{
+  String username;
+  String phone;
+  User({required this.username,required this.phone});
+}
