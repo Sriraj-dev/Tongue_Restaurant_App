56394e708b9bcb6610039aaae372f9e5c3b28777
Sriraj-dev <Psriraj1902@gmail.com>
2021-11-07 11:30:55 +0530

    Modified body.dart

--- lib/Screens/Body.dart
+++ lib/Screens/Body.dart
@@ -1,12 +1,16 @@
-
-import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:delivery_app/components/Category_Items.dart';
 import 'package:delivery_app/components/SearchBox.dart';
-import 'package:delivery_app/constants.dart';
-import 'package:delivery_app/components/Category_list.dart';
-class Body extends StatelessWidget {
-  const Body({Key? key}) : super(key: key);
+import 'package:flutter/material.dart';
 
+class Body extends StatefulWidget {
+  //const Body({Key? key}) : super(key: key);
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  List<bool> activeItems = [true,false,false,false,false];
+  String title = 'Biryani';
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -14,8 +18,74 @@ class Body extends StatelessWidget {
         SearchBox(
           onChanged: (value) {},
         ),
-        Category_list(),
+        categoryList(),
+        Center(
+          child: Text(title),
+        )
       ],
     );
+  }
+
+
+
+  SingleChildScrollView categoryList() {
+    return SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            CategoryItem(
+              title: "biryani",
+              isActive: activeItems[0],
+              press: () {
+                setState(() {
+                  activeItems = [true,false,false,false,false];
+                  title = 'biryani';
+                });
+              },
+            ),
+            CategoryItem(
+              title: "chinese",
+              isActive: activeItems[1],
+              press: () {
+                setState(() {
+                  activeItems = [false,true,false,false,false];
+                  title = 'chinese';
+                });
+              },
+            ),
+            CategoryItem(
+              title: "starters",
+              isActive: activeItems[2],
+              press: () {
+                setState(() {
+                  activeItems = [false,false,true,false,false];
+                  title = 'starters';
+                });
+              },
+            ),
+            CategoryItem(
+              title: "Beaverages",
+              isActive: activeItems[3],
+              press: () {
+                setState(() {
+                  activeItems = [false,false,false,true,false];
+                  title = 'Beaverages';
+                });
+              },
+            ),
+            CategoryItem(
+              title: "juices",
+              isActive: activeItems[4],
+              press: () {
+                setState(() {
+                  activeItems = [false,false,false,false,true];
+                  title = 'juices';
+                });
+              },
+            ),
+          ],
+        ),
+      );
   }
 }
--- lib/components/Category_Items.dart
+++ lib/components/Category_Items.dart
@@ -1,6 +1,6 @@
 import 'package:delivery_app/constants.dart';
 import 'package:flutter/material.dart';
-class CategoryItem extends StatelessWidget {
+class CategoryItem extends StatefulWidget {
   final String title;
   final bool isActive;
   final Function press;
@@ -10,29 +10,40 @@ class CategoryItem extends StatelessWidget {
         required this.press,
         required this.title})
       : super(key: key);
+
+  @override
+  _CategoryItemState createState() => _CategoryItemState();
+}
+
+class _CategoryItemState extends State<CategoryItem> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: <Widget>[
-          Text(title,
-              style: isActive
-                  ? TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold,
-                  color: kTextColor)
-                  : TextStyle(fontSize: 16)),
-          if (isActive)
-            Container(
-              width: 22,
-              height: 3,
-              decoration: BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.circular(20),
+      child: GestureDetector(
+        onTap: (){
+          widget.press();
+        },
+        child: Column(
+          children: <Widget>[
+            Text(widget.title,
+                style: widget.isActive
+                    ? TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: kTextColor)
+                    : TextStyle(fontSize: 16)),
+            if (widget.isActive)
+              Container(
+                width: 22,
+                height: 3,
+                decoration: BoxDecoration(
+                  color: Colors.red,
+                  borderRadius: BorderRadius.circular(20),
+                ),
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
