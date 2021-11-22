d4fadb91e21080e17d01446f6d178ee9b937fee5
Sriraj-dev <Psriraj1902@gmail.com>
2021-11-22 20:39:06 +0530

    Fetched UserInfo from backend

--- lib/Screens/ProfileScreen.dart
+++ lib/Screens/ProfileScreen.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app/userModel.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,7 +14,8 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text('This is profile page',
+        child: Text(
+          (username == '')?'User data not available':'User found = $username}',
           style: GoogleFonts.lato(
             fontSize: 17,
           ),
