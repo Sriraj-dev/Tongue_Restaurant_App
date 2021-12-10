5030dc6b9f53ac69b0adefa99ec86bfd57453fe3
jashwanth0712 <74978383+jashwanth0712@users.noreply.github.com>
2021-12-11 00:48:01 +0530

    added help and support page

--- lib/Screens/ProfileScreen.dart
+++ lib/Screens/ProfileScreen.dart
@@ -1,3 +1,4 @@
+import 'package:delivery_app/Screens/help_support.dart';
 import 'package:delivery_app/Services/DBoperations.dart';
 import 'package:delivery_app/Services/localStorage.dart';
 import 'package:delivery_app/Services/storageServices.dart';
@@ -135,41 +136,47 @@ class _ProfileScreenState extends State<ProfileScreen> {
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 8.0, horizontal: 30),
-            child: Container(
-              height: 48,
-              decoration: BoxDecoration(
-                  color: kPrimaryColor.withOpacity(0.06),
-                  borderRadius: BorderRadius.all(Radius.circular(20))),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      SizedBox(
-                        width: 16,
-                      ),
-                      Icon(Icons.help_outline_outlined),
-                      Padding(
-                        padding: const EdgeInsets.only(left: 8.0),
-                        child: Text(
-                          'Help & Support',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              color: kPrimaryColor.withOpacity(0.4),
-                              fontSize: 18),
+            child: GestureDetector(
+              onTap: (){Navigator.push(context,
+                  MaterialPageRoute(builder: (context)=>help_and_support())
+              );
+              },
+              child: Container(
+                height: 48,
+                decoration: BoxDecoration(
+                    color: kPrimaryColor.withOpacity(0.06),
+                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        SizedBox(
+                          width: 16,
                         ),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.navigate_next_rounded),
-                      SizedBox(
-                        width: 16,
-                      ),
-                    ],
-                  ),
-                ],
+                        Icon(Icons.help_outline_outlined),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 8.0),
+                          child: Text(
+                            'Help & Support',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: kPrimaryColor.withOpacity(0.4),
+                                fontSize: 18),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Icon(Icons.navigate_next_rounded),
+                        SizedBox(
+                          width: 16,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
