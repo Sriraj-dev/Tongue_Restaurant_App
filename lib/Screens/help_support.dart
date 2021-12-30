2a133df24ff68c1877d1b214893cd14ec0496f2d
Sriraj-dev <Psriraj1902@gmail.com>
2021-12-30 11:37:29 +0530

    notifications

--- lib/Screens/help_support.dart
+++ lib/Screens/help_support.dart
@@ -4,6 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 import 'package:delivery_app/constants.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:delivery_app/Services/notification.dart';
 
 class help_and_support extends StatefulWidget {
   const help_and_support({Key? key}) : super(key: key);
@@ -14,6 +15,8 @@ class help_and_support extends StatefulWidget {
 
 class _help_and_supportState extends State<help_and_support> {
   @override
+  TextEditingController msg = new TextEditingController();
+  bool sent=false;
   final number = 6281437985;
   Widget build(BuildContext context) {
     return Scaffold(
@@ -107,26 +110,39 @@ class _help_and_supportState extends State<help_and_support> {
                       borderRadius: BorderRadius.all(Radius.circular(20))),
                   child: Column(
                     children: [
+                      (sent==false)?
                       TextField(
                         maxLines: 8,
+                        controller: msg,
                         decoration: InputDecoration(
                           hintText: " Enter your query here ",
                           hintStyle: TextStyle(color: kPrimaryLightColor.withOpacity(0.4)),
                           border: InputBorder.none,
                         ),
-                      ),
+
+
+                      ):
+                      Lottie.asset('assets/tick.json',height: 200,repeat: false),
+
                       Row(mainAxisAlignment: MainAxisAlignment.end,
                         children: [
-                          Container(
-                            decoration:BoxDecoration(
-                            color: kPrimaryColor,
-                                borderRadius: BorderRadius.all(Radius.circular(20))
-                            ),
+                          (sent==false)?
+                          GestureDetector(
+                            onTap:(){NotificationService.sendEmail(message:msg.text); msg.text='';setState(() {
+                              sent=true;
+                            });},
+                            child: Container(
+                              decoration:BoxDecoration(
+                                  color: kPrimaryColor,
+                                  borderRadius: BorderRadius.all(Radius.circular(20))
+                              ),
                               child: Padding(
                                 padding: const EdgeInsets.only(top:8.0,left: 8,right: 6,bottom: 8),
                                 child: Icon(Icons.send_rounded,color: Colors.white,size: 30,),
                               ),
-                          ),
+                            ),
+                          )
+                              :Container(),
                         ],
                       ),
                     ],
